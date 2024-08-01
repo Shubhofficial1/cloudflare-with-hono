bd12bd6fb782eac6978d7a8227cfa23b2697cf74
shubham kumar <shubhofficial1@gmail.com>
2024-08-02 01:08:10 +0530

    Updated code for new method

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", async (c) => {
-  const body = await c.req.json();
+  const body = await c.req.parseBody();
   console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
-
   return c.text("Hello Hono!");
 });
 
